12b99bbd85b30e9eebd8d10a4bba3900fb8b5775
Souvik Naskar <shouvik0104@gmail.com>
2025-03-04 21:56:34 +0530

    first element not smaller than target in sorted array

---
+++ search/first_element_not_smaller.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+
+int first_not_smaller(std::vector<int>&arr, int target) {
+	int left = 0, right = arr.size() - 1;
+	int first_true = -1;
+
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (arr.at(mid) >= target) {
+			first_true = mid;
+			right = mid - 1;
+		} else {
+			left = mid + 1;
+		}
+	}
+
+	return first_true;
+}
+
