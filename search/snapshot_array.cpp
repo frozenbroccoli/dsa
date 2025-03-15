2eca564d4ab3cf353306b8092ff36ef594608cac
Souvik Naskar <shouvik0104@gmail.com>
2025-03-16 02:48:11 +0530

    had to fix snapshot array with binary search ;_; turns out i'm stupid

--- search/snapshot_array.cpp
+++ search/snapshot_array.cpp
@@ -5,7 +5,7 @@
 class SnapshotArray {
 private:
 	int snap_count;
-	std::vector<std::vector<int>> histories;
+	std::vector<std::vector<std::pair<int, int>>> histories;
 
 public:
 	SnapshotArray(int length);
@@ -18,34 +18,47 @@ public:
 
 SnapshotArray::SnapshotArray(int length) {
 	snap_count = 0;
-	histories.assign(length, {0});
+	histories.assign(length, {{0, 0}});
 }
 
 void SnapshotArray::set(int index, int val) {
-	if (snap_count < histories.at(index).size()) {
-		histories.at(index).at(snap_count) = val;
+	std::vector<std::pair<int, int>>& history = histories[index];
+	if (history[history.size() - 1].first == snap_count) {
+		history[history.size() - 1].second = val; 
 	} else {
-		histories.at(index).push_back(val);
+		history.push_back({snap_count, val});
 	}
 }
 
 int SnapshotArray::snap() {
-	return snap_count++;+ 
+	return snap_count++;
 }
 
 int SnapshotArray::get(int index, int snap_id) {
-	if (snap_id >= histories.at(index).size()) {
-		return histories.at(index).at(histories.at(index).size() - 1);
+	std::vector<std::pair<int, int>>& history = histories[index];
+	
+	int left = 0, right = history.size() - 1;
+	int mid;
+	int pos = -1;
+
+	while (left <= right) {
+		mid = left + (right - left) / 2;
+		if (history[mid].first <= snap_id) {
+			pos = mid;
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
 	}
-	return histories.at(index).at(snap_id);
+	return history[pos].second;
 }
 
 void SnapshotArray::debug() {
 	std::cout << "[";
 	for (auto& vec : histories) {
 		std::cout << "[";
-		for (auto& num : vec) {
-			std::cout << num << ", ";
+		for (auto& pair : vec) {
+			std::cout << pair.second << ", ";
 		}
 		std::cout << "], ";
 	}
@@ -55,10 +68,26 @@ void SnapshotArray::debug() {
 
 int main() {
 	SnapshotArray arr = SnapshotArray(3);
+	arr.debug();
+
 	arr.set(0, 5);
+	arr.debug();
+
+	arr.set(0, 9);
+	arr.debug();
+
 	int id0 = arr.snap();
+	arr.debug();
+
 	arr.set(0, 6);
+	arr.debug();
+
+	arr.set(2, 8);
+	arr.debug();
+
 	int id1 = arr.snap();
+	arr.debug();
+
 	std::cout << "id0: " << id0 << " | id1: " << id1 << std::endl;
 	std::cout << arr.get(0, id0) << std::endl;
 	std::cout << arr.get(0, id1) << std::endl;
