265ebd62fb3c60481f24367520429f64d67b23a3
Souvik Naskar <shouvik0104@gmail.com>
2025-03-23 17:28:27 +0530

    happy numbers in constant space

--- two_pointers/happy_numbers.cpp
+++ two_pointers/happy_numbers.cpp
@@ -42,11 +42,35 @@ bool is_happy_number(int n) {
 }
 
 
+int steps(int n) {
+	std::vector<int> digits = parse_digits(n);
+	return sum_of_digits_squared(digits);
+}
+
+
+bool is_happy_number_o1_space(int n) {
+	int tortoise = steps(n);
+	int hare = steps(steps(n));
+
+	while (tortoise != hare && hare != 1) {
+		tortoise = steps(tortoise);
+		hare = steps(steps(hare));
+	}
+	return hare == 1;
+}
+
+
 int main() {
 	std::cout << is_happy_number(20) << std::endl;
 	std::cout << is_happy_number(912) << std::endl;
 	std::cout << is_happy_number(45) << std::endl;
 	std::cout << is_happy_number(1) << std::endl;
 	std::cout << is_happy_number(93783938) << std::endl;
+
+	std::cout << is_happy_number_o1_space(20) << std::endl;
+	std::cout << is_happy_number_o1_space(912) << std::endl;
+	std::cout << is_happy_number_o1_space(45) << std::endl;
+	std::cout << is_happy_number_o1_space(1) << std::endl;
+	std::cout << is_happy_number_o1_space(93783938) << std::endl;
 }
 
