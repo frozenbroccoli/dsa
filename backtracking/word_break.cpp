757c3482fecf330906d21addd100864de2701225
Souvik Naskar <shouvik0104@gmail.com>
2025-04-26 14:39:45 +0530

    can given list of strings be concatenated to form given string

---
+++ backtracking/word_break.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+
+
+bool dfs(int start, std::string& s, std::vector<int>& memo, std::vector<std::string>& words) {
+	if (start == s.size()) return true;
+	if (memo[start] != -1) return memo[start] == 1;
+
+	bool ans = 0;
+	for (std::string& word : words) {
+		if (s.substr(start).find(word) == 0) {
+			if (dfs(start + word.size(), s, memo, words)) {
+				ans = 1;
+				break;
+			}
+		}
+	}
+	memo[start] = ans;
+	return ans == 1;
+}
+
+
+bool word_break(std::string& s, std::vector<std::string>& words) {
+	std::vector<int> memo(s.size(), -1);
+	return dfs(0, s, memo, words);
+}
+
