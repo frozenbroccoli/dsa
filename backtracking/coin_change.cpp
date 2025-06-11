e585430ba39721577527bbee72d44a248b028869
Souvik Naskar <shouvik0104@gmail.com>
2025-06-11 23:29:58 +0530

    correct coin change but without memoization

--- backtracking/coin_change.cpp
+++ backtracking/coin_change.cpp
@@ -1,31 +1,34 @@
-#include <vector>
 #include <algorithm>
-#include <functional>
-#include <climits>
+#include <vector>
 
 
-bool dfs(std::vector<int>& sorted_coins, int& amount, int start, int current_amount, int& result) {
-    if (current_amount == amount) return true;
-    if (current_amount > amount) return false;
-    if (start >= sorted_coins.size()) return false;
+void dfs(
+    const std::vector<int>& coins, 
+    const int& amount, 
+    int& num_coins, 
+    int& sum, 
+    int& result, 
+    int start
+) {
+    if (sum == amount) {
+        result = (result == -1) ? num_coins : std::min(num_coins, result);
+        return;
+    } else if (sum > amount) return;
 
-    for (int greedy_amount, index = start; index < sorted_coins.size(); index++) {
-        for (int multiplier = (amount - current_amount) / sorted_coins[index]; multiplier >= 0; multiplier--) {
-            greedy_amount = current_amount + multiplier * sorted_coins[index];
-            result += multiplier;
-            if (dfs(sorted_coins, amount, index + 1, greedy_amount, result)) return true;
-            result -= multiplier;
-        }
+    for (int i = start; i < coins.size(); i++) {
+        sum += coins[i];
+        num_coins += 1;
+        dfs(coins, amount, num_coins, sum, result, i);
+        sum -= coins[i];
+        num_coins -= 1;
     }
-
-    return false;
 }
 
 
 int coin_change(std::vector<int>& coins, int amount) {
-    std::sort(coins.begin(), coins.end(), std::greater<>());
-    int result {0};
-    if (dfs(coins, amount, 0, 0, result)) return result;
-    return -1;
+    int sum {0}, num_coins {0};
+    int result {-1};
+    dfs(coins, amount, num_coins, sum, result, 0);
+    return result;
 }
 
