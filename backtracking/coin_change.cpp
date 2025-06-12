f8c86253f2c934f569957b1cdc8fc2c4c20776a7
Souvik Naskar <shouvik0104@gmail.com>
2025-06-13 01:21:17 +0530

    pruning with memoization in coin change

--- backtracking/coin_change.cpp
+++ backtracking/coin_change.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 
 
-void dfs(
+bool dfs(
     const std::vector<int>& coins, 
     const int& amount, 
+    std::vector<int>& memo,
     int& num_coins, 
     int& sum, 
     int& result, 
@@ -12,23 +13,34 @@ void dfs(
 ) {
     if (sum == amount) {
         result = (result == -1) ? num_coins : std::min(num_coins, result);
-        return;
-    } else if (sum > amount) return;
+        return true;
+    } else if (sum > amount) return false;
 
+    if (memo[sum] != -1 && memo[sum] <= num_coins) {
+        return false;
+    }
+
+    memo[sum] = num_coins;
+
+    bool ret {false};
     for (int i = start; i < coins.size(); i++) {
         sum += coins[i];
         num_coins += 1;
-        dfs(coins, amount, num_coins, sum, result, i);
+        if (dfs(coins, amount, memo, num_coins, sum, result, i)) {
+            ret = true;
+        }
         sum -= coins[i];
         num_coins -= 1;
     }
+    return ret;
 }
 
 
 int coin_change(std::vector<int>& coins, int amount) {
     int sum {0}, num_coins {0};
+    std::vector<int> memo(amount, -1);
     int result {-1};
-    dfs(coins, amount, num_coins, sum, result, 0);
+    dfs(coins, amount, memo, num_coins, sum, result, 0);
     return result;
 }
 
